ce19df6ebbc41af9c91c154284be7cbff8588742
lailabougria <laila@igindo.com>
2025-02-14 11:11:08 +0100

    add interest calculation exercise

---
+++ designing-complex-business-processes-with-messaging/exercises/interest-calculation/AccountTransactions.Contracts/AccountBlocked.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using NServiceBus;
+
+namespace AccountTransactions.Contracts;
+
+public class AccountBlocked : IEvent
+{
+    public Guid AccountId { get; set; }
+}
---
+++ designing-complex-business-processes-with-messaging/exercises/interest-calculation/AccountTransactions/AccountBalancePolicyData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NServiceBus;
+
+namespace AccountTransactions;
+
+public class AccountBalancePolicyData : ContainSagaData
+{
+    public Guid AccountId { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime NegativeAccountBalanceStartDate { get; set; }
+    public decimal LowestBalance { get; set; }
+}
